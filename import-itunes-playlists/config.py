f482c30d5afe24c7d33081daa91704cd329975a7
Jonathan Backhaus <jonathan.david.backhaus@gmail.com>
2019-05-01 14:57:28 -0400

    Removing 'config.py' files from repo

--- import-itunes-playlists/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-### LOCAL PLEX SETTINGS ###
-PLEX_URL = '<PLEX_URL>'
-PLEX_TOKEN = '<PLEX_TOKEN>'
-USERS = ['<USER>']  # List of users to sync the playlists to
-FILEPATH = '<iTunes Library>.xml'
-PLAYLISTS = ['<PLAYLIST>']
